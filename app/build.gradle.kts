82ec57fb14371521e44e91640c2cb19258c69a42
mahmoud_atallah <m.atallah@silkysys.com>
2024-09-01 14:56:12 +0300

    init as library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,8 @@
+import io.netty.util.ReferenceCountUtil.release
 import org.gradle.internal.impldep.bsh.commands.dir
 
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
     id("maven-publish")
 
@@ -13,11 +14,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.mahmoudatallah.permissions"
         minSdk = 21
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -32,14 +29,24 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
 }
 
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            //from(components["release"])
+            groupId = "com.mahmoudatallah.permissions"
+            artifactId = "permissions"
+            version = "1.0.0"
+        }
+    }
+}
 dependencies {
 
     implementation(libs.androidx.core.ktx)
